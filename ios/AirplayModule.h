cbb0fc668e5cc57a39c844d98ddbead58159ae21
Alessandro Gerelli <geroale2000@gmail.com>
2024-12-19 17:30:41 +0100

    Fix

--- ios/AirplayModule.h
+++ ios/AirplayModule.h
@@ -1,13 +1,6 @@
 #import <React/RCTEventEmitter.h>
-
-#ifdef RCT_NEW_ARCH_ENABLED
-#import "RNAirplayModuleSpec.h"
-
-@interface AirplayModule : RCTEventEmitter <NativeAirplayModuleSpec>
-#else
 #import <React/RCTBridgeModule.h>
 
 @interface AirplayModule : RCTEventEmitter <RCTBridgeModule>
-#endif
 
 @end
